d6e941a1889aaf1c7b4083113332872d04b9f01f
vinayhpl <150535083+vinayhpl@users.noreply.github.com>
2024-10-15 09:00:31 +0530

    Update iihmir.test.js

--- __tests__/iihmir.test.js
+++ __tests__/iihmir.test.js
@@ -1,1 +1,11 @@
+// __tests__/iihmir.test.js
 
+describe('My First Test Suite', () => {
+    test('Example test', () => {
+        expect(true).toBe(true); // Change this to a meaningful test
+    });
+
+    test('Another example test', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
