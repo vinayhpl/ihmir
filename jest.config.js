e6b88bfd86ca89db30a190cc7b286d754e66418c
vinayhpl <150535083+vinayhpl@users.noreply.github.com>
2024-10-16 08:04:47 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,30 @@
+// module.exports = {
+//     collectCoverage: true,
+//     collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'], // Adjust to match your source files
+//     coverageReporters: ['lcov', 'text'],
+//     testMatch: [
+//         '**/__tests__/**/*.[jt]s?(x)', // Matches test files
+//         '**/?(*.)+(spec|test).[jt]s?(x)'
+//     ],
+//     // Add other configurations as needed
+// };
+
 module.exports = {
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'], // Adjust to match your source files
-    coverageReporters: ['lcov', 'text'],
-    testMatch: [
-        '**/__tests__/**/*.[jt]s?(x)', // Matches test files
-        '**/?(*.)+(spec|test).[jt]s?(x)'
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx}',
+        '!src/index.js',
+        '!**/node_modules/**',
     ],
-    // Add other configurations as needed
+
+   coverageReporters: [
+        'text',          // Console output
+        'lcov',          // LCOV report for SonarQube
+        'html',          // HTML report for browser viewing
+        'json'           // JSON report for programmatic access
+    ],
+    verbose: true, // Enable verbose output
+    reporters: ['default', 'jest-silent-reporter'], // Add silent reporter for better error logging
 };
+
